77ec1456e051b2811b7c6c8f3f7b775d5dcfb2c1
Tehreem Masroor <tehreem.masroor@gmail.com>
2025-02-23 13:14:17 +0300

    Cleared the structure for lab 4 part 1

--- tests/test_serialVersion.py
+++
@@ -1,9 +0,0 @@
-from src.serialVersion import serial_main
-
-def test_serial_main():
-    # Test with a smaller number for faster execution during testing
-    assert serial_main(100) > 0  # Ensures that the function runs and returns a positive time
-    assert serial_main(1_000) > 0  
-    assert serial_main(10_000) > 0  
-    assert serial_main(1_000_000) > 0  
-    assert serial_main(100_000_000) > 0  
--- tests/test_sum_num.py
+++
@@ -1,8 +0,0 @@
-from src.sum_num import compute_sum
-
-def test_compute_sum():
-    assert compute_sum(1, 10) == 55
-    assert compute_sum(1, 5) == 15
-    assert compute_sum(10, 20) == 165
-    assert compute_sum(100, 100) == 100
-    assert compute_sum(0, 0) == 0
