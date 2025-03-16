8a9510cbaff2660334dea835574c814fbc7ab10d
Tehreem Masroor <tehreem.masroor@gmail.com>
2025-03-16 11:41:46 +0300

    Ran celery worker with backend, main.py and rabbitmq running in three different terminals

--- src/tasks.py
+++ src/tasks.py
@@ -1,6 +1,10 @@
 from celery import Celery
 
-app = Celery("tasks", broker = "pyamqp://guest@localhost//") #name the file the same as the first argument
+app = Celery(
+    "tasks", 
+    broker = "pyamqp://guest@localhost//", #name the python file the same as the first argument
+    backend="redis://localhost:6379/0"
+)
 
 @app.task
 def power(number, exponent):
