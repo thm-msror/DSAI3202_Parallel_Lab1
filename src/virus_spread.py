157eaf8413d6e6c70f25e779961b00b78d63986d
Tehreem Masroor <tehreem.masroor@gmail.com>
2025-03-16 09:39:14 +0300

    Added files for lab6 part 2

---
+++ src/virus_spread.py
@@ -0,0 +1,9 @@
+# Virus spread function
+def spread_virus(population, spread_chance, vaccination_rate):
+    new_population = population.copy()
+    for i in range(len(population)):
+        if population[i] == 1:  # If infected
+            for j in range(len(population)):
+                if population[j] == 0 and np.random.rand() < spread_chance * (1 - vaccination_rate):
+                    new_population[j] = 1  # Infecting the person
+    return new_population
