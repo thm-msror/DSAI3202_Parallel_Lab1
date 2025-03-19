eb6b2210b9d1cf96afd51575ac1d1291b3a44844
Tehreem Masroor <tehreem.masroor@gmail.com>
2025-03-19 11:35:53 +0300

    Instructions on how to run celery

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import time
 from src.tasks import power
 from src.dispatch_tasks import dispatch
 
 if __name__ == "__main__":
+    start = time.time()
     results = dispatch()
+    end = time.time()
+    print(f"Execution Time: {end - start:.4f} seconds")
     print(results[:10])
+    
+# Run RabbitMQ on PC cmd using the command ssh -L 15672:localhost:15672 user@10.102.10.67
+# Run  celery -A src.tasks worker --loglevel=info in VS code terminal
+# In Another terminal run python main.py
